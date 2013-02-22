d2a445f3dafdc60675c9ae06563e291a4ca738ab
Luis <lrmr1976@gmail.com>
2013-02-22 17:04:40 +0100

    Actual

---
+++ src/es/experiences/model/Box.java
@@ -0,0 +1,70 @@
+package es.experiences.model;
+
+public class Box {
+	
+	private String id;
+	
+	private String name;
+	
+	private String title;
+	
+	private boolean activated; // false by default
+
+	
+	public Box() {
+		// Constructor por defecto
+		super();
+	}
+
+
+	public Box(String id, String name, String title, boolean activated) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.title = title;
+		this.activated = activated;
+	}
+
+
+	public String getId() {
+		return id;
+	}
+
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getTitle() {
+		return title;
+	}
+
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+
+	public boolean isActivated() {
+		return activated;
+	}
+
+
+	public void setActivated(boolean activated) {
+		this.activated = activated;
+	}
+	
+	
+	
+}
